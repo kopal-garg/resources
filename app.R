1fa1f38cdb240caa65532b8fc71ed7f8f90592ff
kopal-garg <42230406+kopal-garg@users.noreply.github.com>
2020-03-28 11:45:55 -0600

    Update app.R

--- app.R
+++ app.R
@@ -91,95 +91,7 @@ server <- function(input, output, session) {
     read.csv(input$v3$datapath)
   })
   update_choices <- function() {
-    # gene= input$genes[1]
-    # enst <- read_tsv('https://raw.githubusercontent.com/kopal-garg/resources/master/by_gene_enst.tsv')
-    # enst_row <- enst %>% dplyr::filter(enst$gene==input$genes[1])
-    # edb <- EnsDb.Hsapiens.v86
-    # txs <- transcripts(edb, filter = GeneNameFilter(input$genes[1]),
-    #                    columns = c("protein_id", "tx_biotype",'uniprot_id'))
-    # up_txs<-data.frame(uniprot=txs$uniprot_id,tx_id=txs$tx_id,start=txs@ranges@start,end=txs@ranges@start+txs@ranges@width)
-    # gns <- up_txs %>% dplyr::filter(up_txs$tx_id == enst_row$transcript)
-    # gns <- subset(gns, select=-c(start,end))
-    # rel_json <- drawProteins::get_features(gns$uniprot[1])
-    # drawProteins::feature_to_dataframe(rel_json) -> rel_data
-    # Category <- lapply(rel_json[[1]][["features"]], function(x) x$category) %>% unlist()
-    # rel_data <- cbind(Category, rel_data)
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$type!='VARIANT')
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$type!='CONFLICT')
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$description!='NONE')
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$type!='MOD_RES')
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$type!='COMPBIAS')
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$type!='REGION')
-    # rel_data <- rel_data %>% dplyr::filter(rel_data$type!='CROSSLNK')
-    # rel_data_m <- rel_data %>% dplyr::filter(rel_data$Category == 'MOLECULE_PROCESSING')
-    # rel_data_d <- rel_data %>% dplyr::filter(rel_data$Category == 'DOMAINS_AND_SITES')
-    # if (nrow(rel_data_m)==0){rel_data <- rel_data_d}
-    # if (nrow(rel_data_d)==0){rel_data <- rel_data_m}
-    # if (nrow(rel_data_m)>0 && nrow(rel_data_d)>0){rel_data <- rbind(rel_data_m,rel_data_d)}
-    # #rel_data$type=str_replace(rel_data$type,'CHAIN','Scaffold')
-    # 
-    # if(is.null(input$v3) | is.null(input$v2)) {
-    #   transcript=enst_row$transcript
-    #   q = paste0('dbnsfp.ensembl.transcriptid:', transcript)
-    #   a_0_1000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=0)
-    #   a_1001_2000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=1001)
-    #   a_2001_3000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=2001)
-    #   a_3001_4000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=3001)
-    #   a_4001_5000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=4001)
-    #   a_5001_6000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=5001)
-    #   a_6001_7000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=6001)
-    #   a_7001_8000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=7001)
-    #   a_8001_9000<-queryVariant(q=q,fields='vcf,gnomad_exome.ac.ac,gnomad_genome.ac.ac,snpeff.ann.effect,snpeff.ann.protein.position,snpeff.ann.protein.length,mutdb.uniprot_id,vcf',dotfield=T,size=1000,from=8001)
-    #   
-    #   a_0_1000 <- subset(a_0_1000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_1001_2000 <- subset(a_1001_2000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_2001_3000 <- subset(a_2001_3000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_3001_4000 <- subset(a_3001_4000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_4001_5000 <- subset(a_4001_5000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_5001_6000 <- subset(a_5001_6000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_6001_7000 <- subset(a_6001_7000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_7001_8000 <- subset(a_7001_8000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    #   a_8001_9000 <- subset(a_8001_9000$hits, select=c("_id","_score","snpeff._license",'snpeff.ann.effect','snpeff.ann.protein.length','snpeff.ann.protein.position','vcf.alt','vcf.ref','vcf.position','gnomad_exome._license','gnomad_exome.ac.ac','gnomad_genome.ac.ac','gnomad_exome._license','gnomad_genome._license'))
-    # 
-    #   a <- rbind(a_0_1000,a_1001_2000, a_2001_3000, a_3001_4000, a_4001_5000,a_5001_6000,a_6001_7000,a_7001_8000,a_8001_9000)
-    #   a <- a %>% dplyr::filter(a$gnomad_genome.ac.ac>0 | a$gnomad_exome.ac.ac>0)
-    #   a$gnomad_exome.ac.ac[is.na(a$gnomad_exome.ac.ac)] <- 0
-    #   a$gnomad_genome.ac.ac[is.na(a$gnomad_genome.ac.ac)] <-0
-    #   a$Allele_Count <- a$gnomad_genome.ac.ac + a$gnomad_exome.ac.ac
-    #   for (i in 1:nrow(a)){ 
-    #     if(!is.null(a$snpeff.ann.protein.position[[i]][1])){
-    #       a$pos[i] <-a$snpeff.ann.protein.position[[i]][1]}
-    #     if(is.null(a$snpeff.ann.protein.position[[i]][1])){
-    #       a$pos[i] <- 0}
-    #     a$variant[i] = a$snpeff.ann.effect[[i]][1]
-    #   }
-    #   
-    #   a <- a %>% dplyr::filter(pos!=0) 
-    #   a <- subset(a, select= -c(`_score`,`snpeff._license`,`vcf.alt`,`vcf.position`,`vcf.ref`,`gnomad_exome._license`, `gnomad_genome._license`, `snpeff.ann.protein.length`,`snpeff.ann.protein.position`,`gnomad_exome.ac.ac`,`gnomad_genome.ac.ac`,`snpeff.ann.effect`))
-    #   data_in <- data.frame(
-    #     pos = a$pos,
-    #     AC =a$Allele_Count,
-    #     variant=a$variant
-    #   )
-    #   data_in$pos=as.numeric(levels(data_in$pos))[data_in$pos]
-    #   data=data_in # in-built v2
-    # }
-    # else{
-    #   v2=v2_rawData()
-    #   v3=v3_rawData()
-    #   v2 <- v2 %>% dplyr::filter(as.character(v2$Protein.Consequence)!="")
-    #   v3 <- v3 %>% dplyr::filter(as.character(v3$Protein.Consequence)!="")
-    #   v2 <- wrapper(v2)
-    #   v3 <- wrapper(v3)
-    #   
-    #   data_uploaded <- full_join(v2, v3, by=c("pos","variant")) %>%
-    #     mutate_all(., ~replace_na(., 0)) %>%
-    #     mutate(AC = Allele.Count.x + Allele.Count.y) %>%
-    #     group_by(pos)
-    #   
-    #   data=data_uploaded
-    #   
-    # }
+
     get_v2_data()
     updateSelectInput(session, 'Variant',choices=c(1:max(data$pos)),selected=1)
     updateSelectInput(session, "DomainChoices",choices = rel_data$type,selected=rel_data$type[1])
@@ -385,14 +297,7 @@ server <- function(input, output, session) {
     
     palette_new <- palette[seq.int(1L, length(palette), 4)]
     
-    # a <- c()
-    # types=unique(domain_df$type)
-    # types = types[types!= 'Scaffold']
-    # for (i in 1:length(types)){
-    #   a$domain_type[i] <- types[i]
-    #   a$color[i] <- palette_new[i]
-    # }
-    # TODO: plot the chain first, remove label and then plot the other domains with labels and labels should be the description 
+
     p=ggplot(data, aes(x = pos, y = 1, size = AC)) +
       geom_rect(domain_df, mapping=aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2,
                                        fill=color), color="black",
